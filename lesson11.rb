8f5b70fffc5d91c4f27c51fd80cbf9b90994a843
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2022-12-12 13:06:49 +0000

    [Finish] 11章 クラスについて学ぼう(3): オーバーライド [演習問題]

--- lesson11.rb
+++ lesson11.rb
@@ -3,14 +3,3 @@ class Car
   puts "車で#{distance}キロ走ります。"
  end
 end
-
-
-class Truck < Car  # Carクラス(親クラス)を継承しているのがTruckクラス(子クラス)
- def run(distance)  # 親クラスで定義されたメソッドを子クラスでも同じ名前で定義して呼び出す
-  super  # Carクラスのrunメソッドを呼び出す
-  puts "大きな荷物を乗せて走ります。"  # Truckクラスに追加したい要素を定義する
- end
-end
-
-truck = Truck.new
-truck.run(5)
